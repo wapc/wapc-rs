6fdab4ca989723d9e858d297551bbc7a4b7c33ff
Kevin Hoffman <alothien@gmail.com>
2020-09-09 10:33:12 -0400

    Adding sync + send to error signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,7 @@
-// Copyright 2015-2020 Capital One Services, LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //! # wapc-guest
 //!
 //! The `wapc-guest` library provides WebAssembly module developers with access to a
-//! [waPC](https://wascap.io/comms)-compliant host runtime. Each guest module has a single
+//! [waPC](https://github.com/wapc)-compliant host runtime. Each guest module has a single
 //! call handler, declared with the `wapc_handler!` macro. Inside this call handler, the guest
 //! module should check the operation of the delivered message and handle it accordingly,
 //! returning any binary payload in response.
