46f7e07914ef3c7c6d5f5248da31867d3e1e5263
Kevin Hoffman <alothien@gmail.com>
2020-09-23 12:39:04 -0400

    first commit

---
+++ src/callbacks.rs
@@ -0,0 +1,216 @@
+use wapc::ModuleState;
+use wasmtime::Memory;
+use wasmtime::{Caller, Func, FuncType, Store, Val, ValType};
+use std::sync::Arc;
+
+
+pub(crate) fn guest_request_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([ValType::I32, ValType::I32]), Box::new([]));
+    Func::new(
+        store,
+        callback_type,
+        move |caller: Caller, params, _results| {
+            let op_ptr = params[0].i32();
+            let ptr = params[1].i32();
+
+            let invocation = host.get_guest_request();
+            let memory = get_caller_memory(&caller).unwrap();
+            if let Some(inv) = invocation {
+                write_bytes_to_memory(memory.clone(), ptr.unwrap(), &inv.msg);
+                write_bytes_to_memory(memory, op_ptr.unwrap(), &inv.operation.as_bytes());
+            }
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn console_log_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([ValType::I32, ValType::I32]), Box::new([]));
+
+    Func::new(
+        store,
+        callback_type,
+        move |caller, params: &[Val], _results: &mut [Val]| {
+            let ptr = params[0].i32();
+            let len = params[1].i32();
+            let memory = get_caller_memory(&caller).unwrap();
+            let vec = get_vec_from_memory(memory, ptr.unwrap(), len.unwrap());
+
+            let msg = std::str::from_utf8(&vec).unwrap();
+
+            host.do_console_log(msg);
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn host_call_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(
+        Box::new([
+            ValType::I32,
+            ValType::I32,
+            ValType::I32,
+            ValType::I32,
+            ValType::I32,
+            ValType::I32,
+            ValType::I32,
+            ValType::I32,
+        ]),
+        Box::new([ValType::I32]),
+    );
+    Func::new(
+        store,
+        callback_type,
+        move |caller: Caller, params: &[Val], results: &mut [Val]| {
+            /*let id = {
+                let mut state = state.borrow_mut();
+                state.host_response = None;
+                state.host_error = None;
+                state.id
+            }; */
+            let memory = get_caller_memory(&caller).unwrap();
+
+            let bd_ptr = params[0].i32();
+            let bd_len = params[1].i32();
+            let ns_ptr = params[2].i32();
+            let ns_len = params[3].i32();
+            let op_ptr = params[4].i32();
+            let op_len = params[5].i32();
+            let ptr = params[6].i32();
+            let len = params[7].i32();
+
+            let vec = get_vec_from_memory(memory.clone(), ptr.unwrap(), len.unwrap());
+            let bd_vec = get_vec_from_memory(memory.clone(), bd_ptr.unwrap(), bd_len.unwrap());
+            let bd = std::str::from_utf8(&bd_vec).unwrap();
+            let ns_vec = get_vec_from_memory(memory.clone(), ns_ptr.unwrap(), ns_len.unwrap());
+            let ns = std::str::from_utf8(&ns_vec).unwrap();
+            let op_vec = get_vec_from_memory(memory, op_ptr.unwrap(), op_len.unwrap());
+            let op = std::str::from_utf8(&op_vec).unwrap();
+            //trace!("Guest {} invoking host operation", id, op);
+            let result = host.do_host_call(bd, ns, op, &vec);
+            if let Ok(r) = result {
+                results[0] = Val::I32(r);
+            }
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn host_response_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([ValType::I32]), Box::new([]));
+    Func::new(
+        store,
+        callback_type,
+        move |caller: Caller, params: &[Val], _results: &mut [Val]| {
+            if let Some(ref e) = host.get_host_response() {
+                let memory = get_caller_memory(&caller).unwrap();
+                let ptr = params[0].i32();
+                write_bytes_to_memory(memory, ptr.unwrap(), &e);
+            }
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn host_response_len_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([]), Box::new([ValType::I32]));
+
+    Func::new(
+        store,
+        callback_type,
+        move |_caller: Caller, _params: &[Val], results: &mut [Val]| {
+            results[0] = Val::I32(match host.get_host_response() {
+                Some(ref r) => r.len() as _,
+                None => 0,
+            });
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn guest_response_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([ValType::I32, ValType::I32]), Box::new([]));
+    Func::new(
+        store,
+        callback_type,
+        move |caller: Caller, params: &[Val], _results: &mut [Val]| {
+            let ptr = params[0].i32();
+            let len = params[1].i32();
+            let memory = get_caller_memory(&caller).unwrap();
+            let vec = get_vec_from_memory(memory, ptr.unwrap(), len.unwrap());
+            host.set_guest_response(vec);
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn guest_error_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([ValType::I32, ValType::I32]), Box::new([]));
+    Func::new(
+        store,
+        callback_type,
+        move |caller: Caller, params: &[Val], _results: &mut [Val]| {
+            let memory = get_caller_memory(&caller).unwrap();
+            let ptr = params[0].i32();
+            let len = params[1].i32();
+
+            let vec = get_vec_from_memory(memory, ptr.unwrap(), len.unwrap());
+            host.set_guest_error(String::from_utf8(vec).unwrap());
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn host_error_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([ValType::I32]), Box::new([]));
+    Func::new(
+        store,
+        callback_type,
+        move |caller: Caller, params: &[Val], _results: &mut [Val]| {
+            if let Some(ref e) = host.get_host_error() {
+                let ptr = params[0].i32();
+                let memory = get_caller_memory(&caller).unwrap();
+                write_bytes_to_memory(memory, ptr.unwrap(), e.as_bytes());
+            }
+            Ok(())
+        },
+    )
+}
+
+pub(crate) fn host_error_len_func(store: &Store, host: Arc<ModuleState>) -> Func {
+    let callback_type = FuncType::new(Box::new([]), Box::new([ValType::I32]));
+    Func::new(
+        store,
+        callback_type,
+        move |_caller: Caller, _params: &[Val], results: &mut [Val]| {
+            results[0] = Val::I32(match host.get_host_error() {
+                Some(ref e) => e.len() as _,
+                None => 0,
+            });
+            Ok(())
+        },
+    )
+}
+
+fn get_caller_memory(caller: &Caller) -> Result<Memory, anyhow::Error> {
+    let memory = caller
+        .get_export("memory")
+        .map(|e| e.into_memory().unwrap());
+    Ok(memory.unwrap())
+}
+
+fn get_vec_from_memory(mem: Memory, ptr: i32, len: i32) -> Vec<u8> {
+    let data = unsafe { mem.data_unchecked_mut() };
+    data[ptr as usize..(ptr + len) as usize]
+        .iter()
+        .copied()
+        .collect()
+}
+
+fn write_bytes_to_memory(memory: Memory, ptr: i32, slice: &[u8]) {
+    let data = unsafe { memory.data_unchecked_mut() };
+    // TODO: upgrade this to a faster memory write
+    for idx in 0..slice.len() {
+        data[idx + ptr as usize] = slice[idx];
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,223 @@
+use wapc::{WebAssemblyEngineProvider, WapcFunctions, ModuleState, HOST_NAMESPACE, WasiParams};
+use std::error::Error;
+use std::rc::Rc;
+use wasmtime::{Instance, Engine, Store, Module, Func, Extern, ExternType};
+use std::cell::RefCell;
+
+// namespace needed for some language support
+const WASI_UNSTABLE_NAMESPACE: &str = "wasi_unstable";
+const WASI_SNAPSHOT_PREVIEW1_NAMESPACE: &str = "wasi_snapshot_preview1";
+
+use crate::modreg::ModuleRegistry;
+use std::sync::Arc;
+
+
+#[macro_use]
+extern crate log;
+
+mod modreg;
+mod callbacks;
+
+macro_rules! call {
+    ($func:expr, $($p:expr),*) => {
+      match $func.call(&[$($p.into()),*]) {
+        Ok(result) => {
+          let result: i32 = result[0].i32().unwrap();
+          result
+        }
+        Err(e) => {
+            error!("Failure invoking guest module handler: {:?}", e);
+            0
+        }
+      }
+    }
+}
+
+struct EngineInner {
+    instance: Rc<RefCell<Option<Instance>>>,
+    guest_call_fn: Func,
+    host: Arc<ModuleState>,
+}
+
+pub struct WasmtimeEngineProvider {
+    inner: Option<EngineInner>,
+    wasidata: Option<WasiParams>,
+    modbytes: Vec<u8>,
+}
+
+impl WasmtimeEngineProvider {
+    pub fn new(buf: &[u8], wasi: Option<WasiParams>) -> WasmtimeEngineProvider {
+        WasmtimeEngineProvider {
+            inner: None,
+            modbytes: buf.to_vec(),
+            wasidata: wasi
+        }
+    }
+}
+
+impl WebAssemblyEngineProvider for WasmtimeEngineProvider {
+    fn init(&mut self, host: Arc<ModuleState>) -> Result<(), Box<dyn Error>> {
+        let instance_ref = Rc::new(RefCell::new(None));
+        let instance = instance_from_buffer(&self.modbytes, &self.wasidata, host.clone())?;
+        instance_ref.replace(Some(instance));
+        let gc = guest_call_fn(instance_ref.clone())?;
+        self.inner = Some(EngineInner {
+            instance: instance_ref,
+            guest_call_fn: gc,
+            host,
+        });
+        self.initialize()?;
+        Ok(())
+    }
+
+    fn call(&mut self, op_length: i32, msg_length: i32) -> Result<i32, Box<dyn Error>> {
+
+        // Note that during this call, the guest should, through the functions
+        // it imports from the host, set the guest error and response
+
+        let callresult: i32 = call!(
+            self.inner.as_ref().unwrap().guest_call_fn,
+            op_length,
+            msg_length
+        );
+
+        Ok(callresult)
+    }
+
+    fn replace(&mut self, module: &[u8]) -> Result<(), Box<dyn Error>> {
+        info!(
+            "HOT SWAP - Replacing existing WebAssembly module with new buffer, {} bytes",
+            module.len()
+        );
+
+        let new_instance = instance_from_buffer(module, &self.wasidata,
+                                                self.inner.as_ref().unwrap().host.clone())?;
+        self.inner.as_ref().unwrap().instance.borrow_mut().replace(new_instance);
+
+        self.initialize()
+    }
+}
+
+impl WasmtimeEngineProvider {
+    fn initialize(&self) -> Result<(), Box<dyn Error>> {
+        if let Some(ext) = self
+            .inner.as_ref().unwrap()
+            .instance
+            .borrow()
+            .as_ref()
+            .unwrap()
+            .get_export("_start")
+        {
+            ext.into_func()
+                .unwrap()
+                .call(&[])?;
+        }
+        Ok(())
+    }
+}
+
+
+fn instance_from_buffer(
+    buf: &[u8],
+    wasi: &Option<WasiParams>,
+    state: Arc<ModuleState>
+) -> Result<Instance, Box<dyn Error>> {
+    let engine = Engine::default();
+    let store = Store::new(&engine);
+    let module = Module::new(&engine, buf).unwrap();
+
+    let d = WasiParams::default();
+    let wasi = match wasi {
+        Some(w) => w,
+        None => &d,
+    };
+
+    // Make wasi available by default.
+    let preopen_dirs =
+        modreg::compute_preopen_dirs(&wasi.preopened_dirs, &wasi.map_dirs).unwrap();
+    let argv = vec![]; // TODO: add support for argv (if applicable)
+
+    let module_registry =
+        ModuleRegistry::new(&store, &preopen_dirs, &argv, &wasi.env_vars).unwrap();
+
+    let imports = arrange_imports(&module, state, store.clone(), &module_registry);
+
+    Ok(wasmtime::Instance::new(&store, &module, imports?.as_slice()).unwrap())
+}
+
+/// wasmtime requires that the list of callbacks be "zippable" with the list
+/// of module imports. In order to ensure that both lists are in the same
+/// order, we have to loop through the module imports and instantiate the
+/// corresponding callback. We **cannot** rely on a predictable import order
+/// in the wasm module
+fn arrange_imports(
+    module: &Module,
+    host: Arc<ModuleState>,
+    store: Store,
+    mod_registry: &ModuleRegistry,
+) -> Result<Vec<Extern>, Box<dyn Error>> {
+    Ok(module
+        .imports()
+        .filter_map(|imp| {
+            if let ExternType::Func(_) = imp.ty() {
+                match imp.module() {
+                    HOST_NAMESPACE => Some(callback_for_import(
+                        imp.name(),
+                        host.clone(),
+                        store.clone(),
+                    )),
+                    // TODO: to forcibly block the use of WASI, these should error
+                    // rather than looking up WASI modules.
+                    WASI_UNSTABLE_NAMESPACE => {
+                        let f = Extern::from(
+                            mod_registry
+                                .wasi_unstable
+                                .get_export(imp.name())
+                                .unwrap()
+                                .clone(),
+                        );
+                        Some(f)
+                    }
+                    WASI_SNAPSHOT_PREVIEW1_NAMESPACE => {
+                        let f: Extern = Extern::from(
+                            mod_registry
+                                .wasi_snapshot_preview1
+                                .get_export(imp.name())
+                                .unwrap()
+                                .clone(),
+                        );
+                        Some(f)
+                    }
+                    other => panic!("import module `{}` was not found", other), //TODO: get rid of panic
+                }
+            } else {
+                None
+            }
+        })
+        .collect())
+}
+
+fn callback_for_import(import: &str, host: Arc<ModuleState>, store: Store) -> Extern {
+    match import {
+        WapcFunctions::HOST_CONSOLE_LOG => callbacks::console_log_func(&store, host.clone()).into(),
+        WapcFunctions::HOST_CALL => callbacks::host_call_func(&store, host.clone()).into(),
+        WapcFunctions::GUEST_REQUEST_FN => callbacks::guest_request_func(&store, host.clone()).into(),
+        WapcFunctions::HOST_RESPONSE_FN => callbacks::host_response_func(&store, host.clone()).into(),
+        WapcFunctions::HOST_RESPONSE_LEN_FN => callbacks::host_response_len_func(&store, host.clone()).into(),
+        WapcFunctions::GUEST_RESPONSE_FN => callbacks::guest_response_func(&store, host.clone()).into(),
+        WapcFunctions::GUEST_ERROR_FN => callbacks::guest_error_func(&store, host.clone()).into(),
+        WapcFunctions::HOST_ERROR_FN => callbacks::host_error_func(&store, host.clone()).into(),
+        WapcFunctions::HOST_ERROR_LEN_FN => callbacks::host_error_len_func(&store, host.clone()).into(),
+        _ => unreachable!(),
+    }
+}
+
+// Called once, then the result is cached. This returns a `Func` that corresponds
+// to the `__guest_call` export
+fn guest_call_fn(instance: Rc<RefCell<Option<Instance>>>) -> Result<Func, Box<dyn Error>> {
+    if let Some(func) = instance.borrow().as_ref().unwrap().get_func(WapcFunctions::GUEST_CALL) {
+        Ok(func)
+    } else {
+        Err("Guest module did not export __guest_call function!".into())
+    }
+}
